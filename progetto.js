ce4d48ed848dc57a9456cfc7215bbf8ccdc21587
FEDERICO <federico.saggiro@icloud.com>
2024-11-04 10:40:57 +0100

    precedente e successiva

--- progetto.js
+++ progetto.js
@@ -1,30 +1,45 @@
+const precedente = document.querySelector(".precedente");
+const successivo = document.querySelector(".successiva")
+
+precedente.onclick=()=>{
+    let giorno = giorno_iniziale()
+    giorno.setDate(giorno.getDate() - 7);
+    table.creaheader(giorno)
+}
+
+successivo.onclick=()=>{
+    let giorno = giorno_iniziale()
+    giorno.setDate(giorno.getDate() + 7);
+    table.creaheader(giorno)
+}
+
+
+const giorno_iniziale = () => {
+    let oggi = new Date();
+    let giorno_settimanale = oggi.getDay();
+
+    if (giorno_settimanale === 6){
+        oggi.setDate(oggi.getDate() + 2);
+    } else if (giorno_settimanale === 0){
+        oggi.setDate(oggi.getDate() + 1);
+    }
+    return oggi;
+};
+
 const createTable = (parentElement) => {
     let data = null;
     let header;
     let newrow = [];
-
-    const giorno_iniziale = () => {
-        let oggi = new Date();
-        let giorno_settimanale = oggi.getDay();
-
-        if (giorno_settimanale === 6){
-            oggi.setDate(oggi.getDate() + 2);
-        } else if (giorno_settimanale === 0){
-            oggi.setDate(oggi.getDate() + 1);
-        }
-        return oggi;
-    }
 
     return {
         build:(dati) => {
             data = dati;
         },
         
-        creaheader:() => {
+        creaheader:(giorno) => {
             header = "<table class='table' border='1'><thead>";
             header += "<th>ORE</th>";
 
-            let giorno = giorno_iniziale();
             header += data.map(day => {
                 const formato = day + " " + giorno.toLocaleDateString("it-IT");
                 giorno.setDate(giorno.getDate() + 1);
@@ -51,6 +66,7 @@ const createTable = (parentElement) => {
 
 let table = createTable(document.querySelector("#table"));
 table.build( ["LUNEDÌ", "MARTEDÌ", "MERCOLEDÌ", "GIOVEDÌ", "VENERDÌ"]);
-table.creaheader();
+let giorno = giorno_iniziale()
+table.creaheader(giorno);
 let hours = ["8", "9", "10", "11", "12"];
 table.crea(5, hours);
